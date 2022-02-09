fe3fd29b20837bf599cfeec80fe9310ea75e939f
stian.oek <stian.oek@gmail.com>
2022-02-10 00:19:18 +0100

    test

--- client/__tests__/home.test.jsx
+++ client/__tests__/home.test.jsx
@@ -1,35 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { MemoryRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { MemoryRouter } from 'react-router-dom';
+import Home from '../src/pages/Home';
 
-import Question from "../src/pages/Question";
-import Home from "../src/pages/Home";
-import { QuestionContext } from "../src/pages/Question";
-import { Simulate } from "react-dom/test-utils";
-
-const question = {
-  question: "Are you happy?",
-  answers: {
-    answer_a: "Yes",
-    answer_b: "No",
-    answer_c: "Maybe",
-  },
-  correct_answers: {
-    answer_a_correct: "true",
-    answer_b_correct: "false",
-    answer_c_correct: "false",
-  },
-};
-
-describe("Quiz application", () => {
-  it("should show home component", () => {
-    const element = document.createElement("div");
-    ReactDOM.render(
-      <MemoryRouter>
-        <Home />
-      </MemoryRouter>,
-      element
-    );
-    expect(element.innerHTML).toMatchSnapshot();
-  });
+describe('Quiz application', () => {
+	it('should show home component', () => {
+		const element = document.createElement('div');
+		ReactDOM.render(
+			<MemoryRouter>
+				<Home />
+			</MemoryRouter>,
+			element
+		);
+		expect(element.innerHTML).toMatchSnapshot();
+	});
 });
