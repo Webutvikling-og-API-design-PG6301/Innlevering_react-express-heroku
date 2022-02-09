39e6641ae52dbeac3684ac00b26e2fd31e9ac9a5
stian.oek <stian.oek@gmail.com>
2022-02-10 00:29:09 +0100

    test

--- client/__tests__/home.test.jsx
+++ client/__tests__/home.test.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import { MemoryRouter } from 'react-router-dom';
 import Home from '../src/pages/Home';
 
