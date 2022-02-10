3b69200c61623839b78da3089f23ca4f4500bad6
StianOek <66445281+StianOek@users.noreply.github.com>
2022-02-10 08:09:30 +0100

    test

--- client/__tests__/home.test.jsx
+++ client/__tests__/home.test.jsx
@@ -1,17 +1,29 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { MemoryRouter } from 'react-router-dom';
-import Home from '../src/pages/Home';
+import React from "react";
+import ReactDOM from "react-dom";
+import { MemoryRouter } from "react-router-dom";
+import Answer from "../src/pages/Answer";
+import Home from "../src/pages/Home";
 
-describe('Quiz application', () => {
-	it('should show home component', () => {
-		const element = document.createElement('div');
-		ReactDOM.render(
-			<MemoryRouter>
-				<Home />
-			</MemoryRouter>,
-			element
-		);
-		expect(element.innerHTML).toMatchSnapshot();
-	});
+describe("Quiz application", () => {
+  it("should show home component", () => {
+    const element = document.createElement("div");
+    ReactDOM.render(
+      <MemoryRouter>
+        <Home />
+      </MemoryRouter>,
+      element
+    );
+    expect(element.innerHTML).toMatchSnapshot();
+  });
+
+  it("should show answers", () => {
+    const element = document.createElement("div");
+    ReactDOM.render(
+      <MemoryRouter>
+        <Answer isRightAnswer={3} isAnsweredQuestion={5} />
+      </MemoryRouter>,
+      element
+    );
+    expect(element.innerHTML).toMatchSnapshot();
+  });
 });
