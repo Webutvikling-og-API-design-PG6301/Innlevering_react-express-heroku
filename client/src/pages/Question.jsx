f174c14739bde718d9114bb1b7c9fee10835818d
stian.oek <stian.oek@gmail.com>
2022-02-12 00:42:38 +0100

    change to api call

--- client/src/pages/Question.jsx
+++ client/src/pages/Question.jsx
@@ -1,57 +1,53 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router";
-import { randomQuestion, isCorrectAnswer } from "../components/questions";
-
-export const QuestionContext = React.createContext({ randomQuestion });
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router';
 
 const Question = ({ setIsRightAnswer, setIsAnsweredQuestion }) => {
-  const [question, setQuestion] = useState("");
-  const navigate = useNavigate();
+	const [question, setQuestion] = useState();
+	const navigate = useNavigate();
 
-  useEffect(() => {
-    loadData();
-  }, []);
+	useEffect(() => {
+		loadData();
+	}, []);
 
-  const loadData = async () => {
-    const response = await fetch("/api/question");
-    const data = await response.json();
-    setQuestion(await data);
-  };
+	const loadData = async () => {
+		const response = await fetch('/api/question');
+		const data = await response.json();
+		console.log(data);
+		setQuestion(await data);
+	};
 
-  const handleRightAnswer = (answer) => {
-    if (isCorrectAnswer(question, answer)) {
-      console.log("right answer");
-      setIsRightAnswer((prev) => prev + 1);
-      setIsAnsweredQuestion((prev) => prev + 1);
-      navigate("/answer/correct");
-    } else {
-      console.log("Wrong answer");
-      setIsAnsweredQuestion((prev) => prev + 1);
-      navigate("/answer/wrong");
-    }
-  };
+	const handleRightAnswer = (answer) => {
+		if (isCorrectAnswer(question, answer)) {
+			console.log('right answer');
+			setIsRightAnswer((prev) => prev + 1);
+			setIsAnsweredQuestion((prev) => prev + 1);
+			navigate('/answer/correct');
+		} else {
+			console.log('Wrong answer');
+			setIsAnsweredQuestion((prev) => prev + 1);
+			navigate('/answer/wrong');
+		}
+	};
 
-  if (!question) {
-    return <h1>Loading...</h1>;
-  }
+	if (!question) {
+		return <h1>Loading...</h1>;
+	}
 
-  console.log(question);
-  return (
-    <div>
-      <h1>{question.question}</h1>
-      {Object.keys(question.answers)
-        .filter((answer) => question.answers[answer])
-        .map((value) => {
-          return (
-            <div key={value} data-testid={value}>
-              <button onClick={() => handleRightAnswer(value)}>
-                {question.answers[value]}
-              </button>
-            </div>
-          );
-        })}
-    </div>
-  );
+	console.log(question);
+	return (
+		<div>
+			<h1>{question.question}</h1>
+			{Object.keys(question.answers)
+				.filter((answer) => question.answers[answer])
+				.map((value) => {
+					return (
+						<div key={value}>
+							<button>{question.answers[value]}</button>
+						</div>
+					);
+				})}
+		</div>
+	);
 };
 
 export default Question;
--- server/server.js
+++ server/server.js
@@ -1,20 +1,20 @@
-import express from "express";
-import path from "path";
-import { randomQuestion } from "./quiz.js";
+import express from 'express';
+import path from 'path';
+import { randomQuestion } from './quiz.js';
 
 const app = express();
 
-app.get("/api/question", (req, res) => {
-  const data = randomQuestion();
-  res.json(data);
+app.get('/api/question', (req, res) => {
+	const { id, question, answers, category } = randomQuestion();
+	res.json({ id, question, answers, category });
 });
 
-app.use(express.static("../client/dist"));
+app.use(express.static('../client/dist'));
 app.use((req, res, next) => {
-  res.sendFile(path.resolve("../client/dist/index.html"));
+	res.sendFile(path.resolve('../client/dist/index.html'));
 });
 const PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, () => {
-  console.log("Server is running on http://localhost:" + server.address().port);
+	console.log('Server is running on http://localhost:' + server.address().port);
 });
